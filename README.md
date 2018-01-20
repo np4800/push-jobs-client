7c9dc382cbe8df4f6c7b6cb0ba9898fbde99f4a6
nikhilkumar-pra-patel <nikhilkumar.pra.patel@hpe.com>
2018-01-20 08:17:05 +0530

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# push-jobs-wrapper
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: push-jobs-wrapper
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
